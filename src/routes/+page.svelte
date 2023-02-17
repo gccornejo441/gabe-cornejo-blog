bec12d1e659e80538337c9d1419c603b642efbaa
Gabriel Cornejo <gcornejo441@gmail.com>
2023-02-17 13:27:21 -0800

    Adding layout pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,47 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import './home/common.css';
+</script>
+
+<svelte:head>
+	<title>SvelteKit • Web development, streamlined</title>
+
+	<meta name="twitter:title" content="SvelteKit" />
+	<meta name="twitter:description" content="Web development, streamlined" />
+	<meta name="description" content="SvelteKit is the official Svelte application framework" />
+</svelte:head>
+
+<div class="home">
+	<h1 class="visually-hidden">SvelteKit</h1>
+
+	<footer>
+		<p>
+			SvelteKit is <a target="_blank" rel="noreferrer" href="https://github.com/sveltejs/kit"
+				>free and open source software</a
+			> released under the MIT license.
+		</p>
+	</footer>
+</div>
+
+<style>
+	footer {
+		padding: 1em var(--sk-page-padding-side);
+		text-align: center;
+		background: var(--sk-back-2);
+	}
+
+	footer p {
+		max-width: 20em;
+		margin: 0 auto;
+	}
+
+	footer p a {
+		color: inherit;
+		text-decoration: underline;
+	}
+
+	@media (min-width: 680px) {
+		footer p {
+			max-width: none;
+		}
+	}
+</style>
